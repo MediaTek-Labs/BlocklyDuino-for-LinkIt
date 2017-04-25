adbabe790de4554c6dee382ebccb3c43dbb418af
Cai <zw.cai@mediatek.com>
2017-04-25 15:10:29 +0800

    Update css, LBLEPeripheral example.

--- examples/LBLEPeripheral/LBLEPeripheral.ino
+++ examples/LBLEPeripheral/LBLEPeripheral.ino
@@ -2,13 +2,14 @@
 #include <LBLEPeriphral.h>
 
 LBLEService __periphralService("951A6899-6F0F-456F-8AE0-5E0B4352D85B");
-LBLECharacteristicInt __periphralCharacteristic("B77D53AC-D590-41E0-B75A-6DA442F543F6", LBLE_READ | LBLE_WRITE);
+LBLECharacteristicInt __b77d53ac_d590_41e0_b75a_6da442f543f6("B77D53AC-D590-41E0-B75A-6DA442F543F6", LBLE_WRITE);
 void setup()
 {
   LBLE.begin();
   while (!LBLE.ready()) { delay(1000); }
 
-  __periphralService.addAttribute(__periphralCharacteristic);
+  __periphralService.addAttribute(__b77d53ac_d590_41e0_b75a_6da442f543f6);
+
   LBLEPeripheral.addService(__periphralService);
   LBLEPeripheral.begin();
   LBLEAdvertisementData __advertisement;
@@ -21,16 +22,15 @@ void setup()
 
 void loop()
 {
-  delay(100);
-  if (__periphralCharacteristic.isWritten()) {
-    if ((int) __periphralCharacteristic.getValue() > 0) {
-      digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+    if (__periphralCharacteristic.isWritten()) {
+      if (__periphralCharacteristic.getValue() == 0) {
+        digitalWrite(LED_BUILTIN, LOW);
 
-    } else {
-      digitalWrite(LED_BUILTIN, LOW);
+      } else {
+        digitalWrite(LED_BUILTIN, HIGH);
+
+      }
 
     }
-
-  }
-
 }
