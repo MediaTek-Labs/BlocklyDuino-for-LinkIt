fc80147c3f5bed6a3fe28174c5e25b044c5db301
Zhi-Wei Cai <support@vox.vg>
2017-05-22 13:04:52 +0800

    Initial blocks moves to (80,40).

--- offline-editor/js/editor.js
+++ offline-editor/js/editor.js
@@ -46,11 +46,11 @@ function newFile() {
   hasWriteAccess = false;
   Entryflg = 0;
   handleDocumentChange(null);
-  var xmlDoc = Blockly.Xml.textToDom('<xml xmlns="http://www.w3.org/1999/xhtml"><block type="initializes_setup" id="0" x="450" y="10"><next><block type="initializes_loop" id="1"></block></next></block></xml>');
+  var xmlDoc = Blockly.Xml.textToDom('<xml xmlns="http://www.w3.org/1999/xhtml"><block type="initializes_setup" id="0" x="80" y="40"><next><block type="initializes_loop" id="1"></block></next></block></xml>');
   var checkInit = function(){
     if(Blockly.mainWorkspace == null){
   	  setTimeout(checkInit, 200);
-  	} else { 
+  	} else {
   	  Blockly.Xml.domToWorkspace(Blockly.mainWorkspace, xmlDoc);
   	}
   };
