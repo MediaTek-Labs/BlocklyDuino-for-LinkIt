3d8f7fb89ddca4030278cba0e8ddacb21da57a20
Cai <zw.cai@mediatek.com>
2017-06-02 09:45:19 +0800

    Issue #57.

--- blockly/msg/js/en.js
+++ blockly/msg/js/en.js
@@ -1105,16 +1105,16 @@ Blockly.Msg.CONTROLS_IF_ELSE_TITLE_ELSE = Blockly.Msg.CONTROLS_IF_MSG_ELSE;
 
 // LinkIt
 Blockly.Msg.CATEGORY_LINKIT = "LinkIt 7697";
-// WiFi
-Blockly.Msg.CATEGORY_LINKIT_WIFI = "WiFi";
+// Wi-Fi
+Blockly.Msg.CATEGORY_LINKIT_WIFI = "Wi-Fi";
 Blockly.Msg.CATEGORY_LINKIT_WIFI_DISCONNECT = "Disconnect Wi-Fi";
-Blockly.Msg.CATEGORY_LINKIT_WIFI_ADVANCED = "WiFi (Advanced)";
+Blockly.Msg.CATEGORY_LINKIT_WIFI_ADVANCED = "Wi-Fi (Advanced)";
 Blockly.Msg.LINKIT_SET_WIFI_HELPURL = "";
-Blockly.Msg.LINKIT_SET_WIFI_TITLE = "WiFi Setup";
-Blockly.Msg.LINKIT_SET_WIFI_SSID = "WIFI ID";
-Blockly.Msg.LINKIT_SET_WIFI_PASSWORD = "WIFI Password";
+Blockly.Msg.LINKIT_SET_WIFI_TITLE = "Wi-Fi Setup";
+Blockly.Msg.LINKIT_SET_WIFI_SSID = "Wi-Fi ID";
+Blockly.Msg.LINKIT_SET_WIFI_PASSWORD = "Wi-Fi Password";
 Blockly.Msg.LINKIT_SET_WIFI_TOOLTIP = "";
-Blockly.Msg.LINKIT_SET_WIFI_STATUS_NO_SHIELD = "No WiFi Sheld";
+Blockly.Msg.LINKIT_SET_WIFI_STATUS_NO_SHIELD = "No Wi-Fi Sheld";
 Blockly.Msg.LINKIT_SET_WIFI_STATUS_IDLE = "Idle";
 Blockly.Msg.LINKIT_SET_WIFI_STATUS_NO_SSID_AVAIL = "No SSID Available";
 Blockly.Msg.LINKIT_SET_WIFI_STATUS_SCAN_COMPLETED = "Scan Completed";
@@ -1122,7 +1122,7 @@ Blockly.Msg.LINKIT_SET_WIFI_STATUS_CONNECTED = "Connected";
 Blockly.Msg.LINKIT_SET_WIFI_STATUS_CONNECT_FAILED = "Connect Failed";
 Blockly.Msg.LINKIT_SET_WIFI_STATUS_CONNECTION_LOST = "Connection Lost";
 Blockly.Msg.LINKIT_SET_WIFI_STATUS_DISCONNECTED = "Disconnected";
-Blockly.Msg.LINKIT_SET_WIFI_READY_TITLE = "WiFi Connected";
+Blockly.Msg.LINKIT_SET_WIFI_READY_TITLE = "Wi-Fi Connected";
 Blockly.Msg.LINKIT_SET_WIFI_UNTIL_READY_TITLE = "Connect to Wi-Fi AP";
 // MCS
 Blockly.Msg.LINKIT_SET_MCS_HELPURL = "";
